654d3eb78695220717637464d6f1308e4379869f
Erin Cho <echo@crc-dot1x-nat-10-239-249-95.bu.edu>
2025-09-30 20:42:51 -0400

    Adding fields.

--- mp-2/src/App.tsx
+++ mp-2/src/App.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const ParentDiv=styled.div`
     width: 80vw;
     margin: auto;
-    border: 5px babyblue solid;
+    border: 5px black solid;
 `;
 
 export default function App(){
